2d2d4083772b5675bdb2d968301d0b8c0a0af8da
Steller <84358594+StellerEthan@users.noreply.github.com>
2021-06-21 18:07:55 +0000

    check?

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -91,7 +91,7 @@ let scrabbleObj = {
     scoringFuction : scrabbleScore
 };
 
-const scoringAlgorithms = [simpleObj , vowelObj , scrabbleObj];
+const scoringAlgorithms = [simpleObj.scoringFuction , vowelObj.scoringFuction , scrabbleObj.scoringFuction];
 
 function scorerPrompt() {
     let scoreFormat = input.question(`Which scoring algorithm would you like to use?
